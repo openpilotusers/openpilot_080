c4b9f49330344904498c255bc1ce85dfb01293a9
Vehicle Researcher <>
2020-12-09 02:34:43 +0900

    openpilot v0.8.0

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -166,7 +166,7 @@ class DynamicFollow:
 
   def _change_cost(self, libmpc):
     TRs = [0.9, 1.8, 2.7]
-    costs = [1., 0.1, 0.01]
+    costs = [1.10, 0.12, 0.05]
     cost = interp(self.TR, TRs, costs)
 
     # change_time = sec_since_boot() - self.profile_change_time
