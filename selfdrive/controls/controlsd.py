b02f28ff43bfda4f87277bc8583dfa18ab0563a4
Vehicle Researcher <>
2020-12-08 14:42:46 +0900

    openpilot v0.8.0

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -457,7 +457,7 @@ class Controls:
                   'live_tracks': self.sm_smiskol['liveTracks'], 'has_lead': plan.hasLead}
 
     # Gas/Brake PID loop
-    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, plan.vTargetFuture, a_acc_sol, self.CP, extras_loc, self.sm['radarState'])
+    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, plan.vTargetFuture, a_acc_sol, self.CP, plan.hasLead, self.sm['radarState'], extras_loc)
     # Steering PID loop and lateral MPC
     actuators.steer, actuators.steerAngle, lac_log = self.LaC.update(self.active, CS, self.CP, path_plan)
 
