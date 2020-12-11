6a19ca69f8c7ca0db78a54327ea97e85b5990e31
Vehicle Researcher <>
2020-12-11 14:03:43 +0900

    openpilot v0.8.0

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -128,23 +128,14 @@ class LongControl():
 
       output_gb = self.pid.update(self.v_pid, v_ego_pid, speed=v_ego_pid, deadzone=deadzone, feedforward=a_target, freeze_integrator=prevent_overshoot)
 
-      if hasLead and radarState.leadOne.status and 4 <= dRel < 25 and abs(vRel*3.6) <= (dRel+4) and output_gb < 0 and (CS.vEgo * CV.MS_TO_KPH) < 60:
+      if hasLead and radarState.leadOne.status and 6 < dRel < 25 and abs(vRel*3.6) <= (dRel+4) and output_gb < -0.5 and (CS.vEgo * CV.MS_TO_KPH) < 60:
         dfactor = 1
-        dfactor = interp(dRel, [4,10,17,25], [4,3,2.2,1])
-        output_gb *= dfactor
+        dfactor = interp(dRel, [6,20,25], [5,5,1])
+        output_gb -= (dfactor*0.1)
         output_gb = clip(output_gb, -brake_max, gas_max)
-      #elif hasLead and radarState.leadOne.status and 4 <= dRel < 15 and 1 > abs(vRel*3.6) > 0 and output_gb >= 0 and (CS.vEgo * CV.MS_TO_KPH) < 30:
-      #  output_gb = 0
-      #  output_gb = clip(output_gb, -brake_max, gas_max)
       elif hasLead and radarState.leadOne.status and 7 < dRel < 17 and abs(vRel*3.6) > 4 and output_gb > 0 and (CS.vEgo * CV.MS_TO_KPH) < 25:
         output_gb *= 1.3
         output_gb = clip(output_gb, -brake_max, gas_max)
-      #elif hasLead and radarState.leadOne.status and 45 < dRel < 100 and abs(vRel*3.6) <= (dRel+3.5) and (CS.vEgo * CV.MS_TO_KPH) > 50 and (vRel*3.6) < -20:
-      #  output_gb -= 1.5
-      #  output_gb = clip(output_gb, -brake_max, gas_max)
-      #if hasLead and radarState.leadOne.status and 4.5 < dRel < 6 and (CS.vEgo * CV.MS_TO_KPH) < (dRel-2) and output_gb < -0.2:
-      #  output_gb += 0.02 * dRel
-      #  output_gb = clip(output_gb, -brake_max, gas_max)
 
       if prevent_overshoot:
         output_gb = min(output_gb, 0.0)
