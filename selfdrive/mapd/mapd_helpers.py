b9d87c8813c80b43254e815cca3b446bd524ca9a
Vehicle Researcher <>
2020-12-17 18:52:36 +0900

    openpilot v0.8.0

--- selfdrive/mapd/mapd_helpers.py
+++ selfdrive/mapd/mapd_helpers.py
@@ -416,7 +416,7 @@ class Way:
         for n in way.way.nodes:
           if 'highway' in n.tags and (n.tags['highway']=='speed_camera'):
             speed_ahead = n.tags['maxspeed'] / 3.6
-            speed_ahead_dist = max(0. , way_pts[count, 0])
+            speed_ahead_dist = max(0. , way_pts[count, 0] - 10.0)
           if 'highway' in n.tags and (n.tags['highway']=='stop' or n.tags['highway']=='give_way' or n.tags['highway']=='mini_roundabout' or (n.tags['highway']=='traffic_signals' and traffic_lights)) and way_pts[count,0] > 0:
             if traffic_status == 'DEAD':
               pass
